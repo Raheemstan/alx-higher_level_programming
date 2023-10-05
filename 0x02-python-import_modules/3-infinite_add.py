15b4df10646df31442a31ce89e5b922d5c8f8f8f
Michael Raheem <abiolar544@gmail.com>
2023-10-05 20:55:15 +0100

    0x02

---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+from sys import argv
+
+args = argv[1:]
+result = sum(int(arg) for arg in args)
+
+print(result)
---
+++ 0x02-python-import_modules/README.md
@@ -0,0 +1,5 @@
+# alx-higher_level_programming
+
+ALX - High Level Language
+***
+0x02 - python-import_modules

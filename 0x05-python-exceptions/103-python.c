da28aee1b716c7fbc8dd4f9284f9f2bc36358790
Michael Raheem <abiolar544@gmail.com>
2023-10-25 19:46:39 +0100

    comments

--- 0x05-python-exceptions/103-python.c
+++ 0x05-python-exceptions/103-python.c
@@ -2,9 +2,11 @@
 #include <stdio.h>
 
 /* Function to print information about a Python list */
-void print_python_list(PyObject *p) {
+void print_python_list(PyObject *p)
+{
     /* Check if the object is a valid Python list */
-    if (!PyList_Check(p)) {
+    if (!PyList_Check(p))
+    {
         printf("[ERROR] Invalid List Object\n");
         return;
     }
@@ -18,16 +20,19 @@ void print_python_list(PyObject *p) {
     printf("[*] Allocated = %ld\n", ((PyListObject *)p)->allocated);
 
     /* Iterate through list elements and print their types */
-    for (Py_ssize_t i = 0; i < size; i++) {
+    for (Py_ssize_t i = 0; i < size; i++)
+    {
         PyObject *item = PyList_GetItem(p, i);
         printf("Element %ld: %s\n", i, Py_TYPE(item)->tp_name);
     }
 }
 
 /* Function to print information about Python bytes */
-void print_python_bytes(PyObject *p) {
+void print_python_bytes(PyObject *p)
+{
     /* Check if the object is a valid Python bytes object */
-    if (!PyBytes_Check(p)) {
+    if (!PyBytes_Check(p))
+    {
         printf("[ERROR] Invalid Bytes Object\n");
         return;
     }
@@ -45,16 +50,19 @@ void print_python_bytes(PyObject *p) {
 
     /* Print the first 10 bytes of the object */
     printf("  first 10 bytes: ");
-    for (Py_ssize_t i = 0; i < 10 && i < size; i++) {
+    for (Py_ssize_t i = 0; i < 10 && i < size; i++)
+    {
         printf("%02x ", (unsigned char)str[i]);
     }
     printf("\n");
 }
 
 /* Function to print information about Python float */
-void print_python_float(PyObject *p) {
+void print_python_float(PyObject *p)
+{
     /* Check if the object is a valid Python float object */
-    if (!PyFloat_Check(p)) {
+    if (!PyFloat_Check(p))
+    {
         printf("[ERROR] Invalid Float Object\n");
         return;
     }

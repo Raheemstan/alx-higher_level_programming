feae28fa6f48046dea3af9d2162c57129fe0b4be
Michael Raheem <abiolar544@gmail.com>
2023-11-04 11:44:46 +0100

    start 0x08

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""
+This module defines a class Rectangle.
+"""
+class Rectangle:
+    """
+    This is an empty class that defines a rectangle.
+    """
+    def __init__(self):
+        pass

5cb4663b6fbc7cf3106fdbf1961eba7f7cf16bcf
Michael Raheem <abiolar544@gmail.com>
2023-11-12 17:43:41 +0100

    base-s5

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,14 +1,27 @@
 #!/usr/bin/python3
+"""Base module"""
 
-"""Module for Base class"""
+import json
+
 class Base:
     """Base class"""
+
     __nb_objects = 0
 
     def __init__(self, id=None):
-        """Initializes the Base class"""
+        """Class constructor"""
+
         if id is not None:
             self.id = id
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """Return the JSON string representation of list_dictionaries"""
+
+        if not list_dictionaries or len(list_dictionaries) == 0:
+            return "[]"
+
+        return json.dumps(list_dictionaries)

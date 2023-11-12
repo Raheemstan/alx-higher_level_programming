ebd0d898c988bb7d082a3c8dad497223db7a44db
Michael Raheem <abiolar544@gmail.com>
2023-11-12 17:45:31 +0100

    base-s7

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -38,3 +38,12 @@ class Base:
             else:
                 list_dicts = [obj.to_dictionary() for obj in list_objs]
                 file.write(cls.to_json_string(list_dicts))
+
+    @staticmethod
+    def from_json_string(json_string):
+        """Return the list of dictionaries represented by json_string"""
+
+        if not json_string:
+            return []
+
+        return json.loads(json_string)

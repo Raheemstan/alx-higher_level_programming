ace92f45dbfc774f52f1233e4d484790a7364148
Michael Raheem <abiolar544@gmail.com>
2024-01-09 16:30:44 +0100

    js check 9-10

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -1,9 +1,19 @@
 #!/usr/bin/node
 
 
-function add (a, b) {
-  const c = a + b;
-  console.log(c);
+function computeFactorial(n) {
+  if (isNaN(n) || n < 0) {
+    console.log(1); // Factorial of NaN or negative number is 1
+  } else if (n === 0 || n === 1) {
+    console.log(1); // Factorial of 0 and 1 is 1
+  } else {
+    console.log(factorial(n));
+  }
 }
 
-add(Number(process.argv[2]), Number(process.argv[3]));
+function factorial(n) {
+  return (n === 0 || n === 1) ? 1 : n * factorial(n - 1);
+}
+
+const arg = parseInt(process.argv[2]);
+computeFactorial(arg);

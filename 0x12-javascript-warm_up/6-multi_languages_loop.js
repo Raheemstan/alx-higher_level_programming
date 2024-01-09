cc95209cad30262b5c0f6535f1da7374f529b6f9
Michael Raheem <abiolar544@gmail.com>
2024-01-09 16:28:45 +0100

    js check 6

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,6 +1,11 @@
 #!/usr/bin/node
 
-const s = ['C is fun', 'Python is cool', 'Javascript is amazing'];
-for (let i = 0; i < 3; i++) {
-  console.log(s[i]);
+const lines = [
+  'C is fun',
+  'Python is cool',
+  'JavaScript is amazing'
+];
+
+for (let i = 0; i < lines.length; i++) {
+  console.log(lines[i]);
 }
